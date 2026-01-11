a9ce3a0a61c6c68426e3a8887ef30f94360f946f
Rockson sena <rocksonsena21@gmail.com>
2026-01-11 18:16:54 +0000

    Fix field name inconsistency in Book model and update admin display settings

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from .models import Book  # relative import is correct
+from .models import Book
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'published_year')
-    list_filter = ('published_year', 'author')
+    # Columns to show in the list view
+    list_display = ('title', 'author', 'publication_year')
+    
+    # Filters on the right-hand side
+    list_filter = ('publication_year', 'author')
+    
+    # Search bar fields
     search_fields = ('title', 'author')
+
