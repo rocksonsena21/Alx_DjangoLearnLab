3af647cf1a66eee8078a65aa4f3c3d197ee5ea43
Rockson sena <rocksonsena21@gmail.com>
2026-01-24 19:31:35 +0000

    Add list_books template and update view to use correct path

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,7 +5,12 @@ from django.views.generic import DetailView
 # Create your views here.
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
+    return render(
+        request,
+        'relationship_app/list_books.html',
+        {'books': books}
+    )
+
 
 
 class LibraryDetailView(DetailView):
