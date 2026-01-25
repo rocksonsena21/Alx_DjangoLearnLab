baf65a9723a2cfaca42492c60b5bf13ab0d95478
Rockson sena <rocksonsena21@gmail.com>
2026-01-25 13:27:37 +0000

    Add user authentication views and templates for login, logout, and registration

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Book, Library
 from django.views.generic.detail import DetailView 
 from .models import Library 
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
+
 
 # Create your views here.
 def list_books(request):
@@ -12,11 +15,27 @@ def list_books(request):
         {'books': books}
     )
 
-
-
 class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"  # ✅ matches checker
     context_object_name = "library"
 
 
+class UserLoginView(LoginView):
+    template_name = "relationship_app/login.html"
+
+
+class UserLogoutView(LogoutView):
+    template_name = "relationship_app/logout.html"
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+    else:
+        form = UserCreationForm()
+
+    return render(request, "relationship_app/register.html", {"form": form})
