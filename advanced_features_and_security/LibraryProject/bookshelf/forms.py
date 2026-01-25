548a5300e807da8641ec2947549ff67b2e7234f6
Rockson sena <rocksonsena21@gmail.com>
2026-01-25 18:13:08 +0000

    Add ExampleForm for book details input in forms.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,7 +1,14 @@
 from django import forms
 from .models import Book
 
+class ExampleForm(forms.Form):
+    title = forms.CharField(max_length=255)
+    author = forms.CharField(max_length=255)
+    published_date = forms.DateField(required=False)
+
+
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'author', 'published_date']
+
