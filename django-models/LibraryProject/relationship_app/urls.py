43555a60a1101a9e161c6f29b5553e05f3a13ece
Rockson sena <rocksonsena21@gmail.com>
2026-01-25 14:54:41 +0000

    Fix URL patterns for book management by updating paths for adding, editing, and deleting books

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -25,10 +25,10 @@ urlpatterns = [
     
     path("member-view/", views.member_view, name="member_view"),
 
-    path("books/add/", views.add_book, name="add_book"),
+    path("books/add_book/", views.add_book, name="add_book"),
 
-    path("books/edit/<int:book_id>/", views.edit_book, name="edit_book"),
-    
-    path("books/delete/<int:book_id>/", views.delete_book, name="delete_book"),
+    path("books/edit_book/<int:book_id>/", views.edit_book, name="edit_book"),
+
+    path("books/delete_book/<int:book_id>/", views.delete_book, name="delete_book"),
 
 ]
