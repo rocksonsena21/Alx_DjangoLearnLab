5acb9f222a8da24a489a39835b9384711f4ad6ed
Rockson sena <rocksonsena21@gmail.com>
2026-02-07 07:58:16 +0000

    Rename view classes for book management to follow naming conventions and improve clarity

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,26 +1,54 @@
 from django.shortcuts import render
-from rest_framework import generics , viewsets
+from rest_framework import generics , permissions
 from .models import Book
 from .serializers import BookSerializer
 
 # Create your views here.
-class Listview(generics.ListAPIView):
+class BookListView(generics.ListAPIView):
+    """
+    Retrieves a list of all books.
+    Accessible to everyone (authenticated or not).
+    """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
 
-class Detailview(generics.RetrieveAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
 
-class Createview(generics.CreateAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
 
-class Updateview(generics.UpdateAPIView):
+class BookDetailView(generics.RetrieveAPIView):
+    """
+    Retrieves a single book using its ID.
+    """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
 
-class Deleteview(generics.DestroyAPIView):
+
+class BookCreateView(generics.CreateAPIView):
+    """
+    Creates a new book.
+    Only authenticated users are allowed.
+    """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class BookUpdateView(generics.UpdateAPIView):
+    """
+    Updates an existing book.
+    Only authenticated users are allowed.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class BookDeleteView(generics.DestroyAPIView):
+    """
+    Deletes an existing book.
+    Only authenticated users are allowed.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
