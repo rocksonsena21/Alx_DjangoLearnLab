e7d46c5cc86c2c970549db83c86cac637b357356
Rockson sena <rocksonsena21@gmail.com>
2026-02-07 07:53:57 +0000

    Refactor view classes for book management to improve consistency and readability

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -4,8 +4,23 @@ from .models import Book
 from .serializers import BookSerializer
 
 # Create your views here.
-class BookList(generics.ListAPIView):
+class Listview(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+class Detailview(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class Createview(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class Updateview(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class Deleteview(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 
