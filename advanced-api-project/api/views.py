8526177cd45cf954198819a4e878e2d82be4cc1c
Rockson sena <rocksonsena21@gmail.com>
2026-02-07 09:06:11 +0000

    Refactor import statement for DjangoFilterBackend for consistency

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,7 +3,7 @@ from rest_framework import generics, filters
 from .models import Book, Author
 from .serializers import BookSerializer, AuthorSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework as DjangoFilterBackend
 
 # Create your views here.
 class AuthorListView(generics.ListAPIView):
