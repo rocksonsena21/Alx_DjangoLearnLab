e7aa36fa170d8026034dcb5fa7b39a22d72417d1
Rockson sena <rocksonsena21@gmail.com>
2026-02-14 09:36:09 +0000

    added basic authentication and created register, login, logout pages and logic

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,6 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import RegisterForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 def home(request):
     return render(request, 'blog/base.html')
+
+
+def posts(request):
+    return render(request, 'blog/posts.html')
+
+
+
+
+def register(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("home")
+
+    else:
+        form = RegisterForm()
+
+    return render(request, "blog/register.html", {"form": form})
+
+
+@login_required
+def profile(request):
+    return render(request, "blog/profile.html")
