c9e6fef46cf1e75e9ab3b72a98e6a078097b2644
Rockson sena <rocksonsena21@gmail.com>
2026-02-15 18:31:36 +0000

    h

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,18 +1,31 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.forms import ModelForm
+
 from .models import Comment
 
+
 class RegisterForm(UserCreationForm):
-    email = forms.EmailField()
+
+    email = forms.EmailField(required=True)
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
 
-class CommentForm(ModelForm):
+class CommentForm(forms.ModelForm):
+
     class Meta:
         model = Comment
         fields = ['content']
+
+    # Simple validation (recommended)
+    def clean_content(self):
+
+        content = self.cleaned_data.get("content")
+
+        if len(content.strip()) < 2:
+            raise forms.ValidationError("Comment is too short.")
+
+        return content
