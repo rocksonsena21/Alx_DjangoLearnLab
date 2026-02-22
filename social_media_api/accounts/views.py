ce5151803bec47189d7a64601774c37b198ccd25
Rockson sena <rocksonsena21@gmail.com>
2026-02-22 23:24:45 +0000

    specific

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth import authenticate
+from django.contrib.auth import get_user_model
 from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.authtoken.models import Token
-from .serializers import RegisterSerializer
+from rest_framework.permissions import IsAuthenticated
+from .serializers import RegisterSerializer, LoginSerializer
 
 
 class RegisterView(generics.CreateAPIView):
@@ -12,18 +14,31 @@ class RegisterView(generics.CreateAPIView):
 
 class LoginView(APIView):
     def post(self, request):
-        username = request.data.get("username")
-        password = request.data.get("password")
+        serializer = LoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        username = serializer.validated_data["username"]
+        password = serializer.validated_data["password"]
 
         user = authenticate(username=username, password=password)
 
         if user:
             token, created = Token.objects.get_or_create(user=user)
-            return Response({
-                "token": token.key
-            })
+            return Response({"token": token.key})
 
         return Response(
             {"error": "Invalid Credentials"},
             status=status.HTTP_400_BAD_REQUEST
         )
+
+
+class ProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        return Response({
+            "id": user.id,
+            "username": user.username,
+            "email": user.email,
+        })
