ed935cef59e237893889d3abe233dd9dc7b6afcc
Rockson sena <rocksonsena21@gmail.com>
2026-02-22 23:27:34 +0000

    i

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -4,7 +4,7 @@ from rest_framework.authtoken.models import Token
 
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField()
 
     class Meta:
         model = get_user_model()
